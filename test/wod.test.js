0fd7ebd558d5c419ab82b901e5070ab079ac3df5
Yassine AZZOUT <92bondstreet@gmail.com>
2014-10-08 06:55:00 +0200

    Update test with not null exercices value Update package.json

--- test/wod.test.js
+++ test/wod.test.js
@@ -26,21 +26,21 @@
  			describe('with no arguments', function() {
 		        it('throw ERROR', function() {
 		          (function () {
-		          	wod.random();		            
-		          }).should.throw();  
+		          	wod.random();
+		          }).should.throw();
 		        });
 		    });
 
 		    describe('More than 2 arguments', function() {
 		        it('throw ERROR', function() {
 		          (function () {
-		           wod.random("","","");		
+		           wod.random("","","");
 		          }).should.throw();
 		        });
 
 		         it('throw ERROR', function() {
 		          (function () {
-		           wod.random("","","","");		
+		           wod.random("","","","");
 		          }).should.throw();
 		        });
 		    });
@@ -48,28 +48,29 @@
 		    describe('With 2 invalids arguments', function() {
 		        it('throw ERROR', function() {
 		          (function () {
-		             wod.random("","");		
+		             wod.random("","");
 		          }).should.throw();
 		        });
 		    });
 
 		    describe('With 1 valids arguments', function() {
-		        //it('return OK if args are correct', function() {                  			
+		        //it('return OK if args are correct', function() {
           		//	wod.random(traceError).should.be.true;
         		//});
 
-		         it('return a random WOD from all categories', function(done) { 
-		         	this.timeout(50000);               			
+		         it('return a random WOD from all categories', function(done) {
+		         	this.timeout(50000);
 		         	var wod_categories = 17;
 		        	wod.random(function(err,workouts){
-             			if (err) 
+             			if (err)
              				console.log(err);
              			else{
              				workouts.should.have.length(wod_categories);
 		          			for(var i=0;i<workouts.length;i++){
-		          				workouts[i].should.have.property('title');	
-		          				workouts[i].should.have.property('tagline');	
-		          				workouts[i].should.have.property('exercices');	
+		          				workouts[i].should.have.property('title');
+		          				workouts[i].should.have.property('tagline');
+		          				workouts[i].should.have.property('exercices');
+											workouts[i]['exercices'].should.be.ok;
 		          			}
              			}
             			done();
@@ -78,21 +79,22 @@
 		    });
 
 			describe('With 2 valids arguments', function() {
-		        it('return OK if args are correct', function() {                  			
+		        it('return OK if args are correct', function() {
           			wod.random(categories,traceError).should.be.true;
         		});
 
-        		it('return WODs according input categories', function(done) {   
-        			this.timeout(50000);               			
+        		it('return WODs according input categories', function(done) {
+        			this.timeout(50000);
         			wod.random(categories,function(err,workouts){
-             			if (err) 
+             			if (err)
              				console.log(err);
              			else{
              				workouts.should.have.length(categories.length);
 		          			for(var i=0;i<workouts.length;i++){
-		          				workouts[i].should.have.property('title');	
-		          				workouts[i].should.have.property('tagline');	
-		          				workouts[i].should.have.property('exercices');	
+		          				workouts[i].should.have.property('title');
+		          				workouts[i].should.have.property('tagline');
+		          				workouts[i].should.have.property('exercices');
+											workouts[i]['exercices'].should.be.ok;
 		          			}
              			}
             			done();
@@ -110,21 +112,21 @@
  			describe('with no arguments', function() {
 		        it('throw ERROR', function() {
 		          (function () {
-		          	wod.getone();		            
-		          }).should.throw();  
+		          	wod.getone();
+		          }).should.throw();
 		        });
 		    });
 
 		    describe('More than 1 arguments', function() {
 		        it('throw ERROR', function() {
 		          (function () {
-		           wod.getone("","","");		
+		           wod.getone("","","");
 		          }).should.throw();
 		        });
 
 		         it('throw ERROR', function() {
 		          (function () {
-		           wod.getone("","","","");		
+		           wod.getone("","","","");
 		          }).should.throw();
 		        });
 		    });
@@ -132,33 +134,34 @@
 		    describe('With 1 invalids arguments', function() {
 		        it('throw ERROR', function() {
 		          (function () {
-		             wod.getone("");		
+		             wod.getone("");
 		          }).should.throw();
 		        });
 		    });
-		    
+
 			describe('With 1 valids arguments', function() {
-		        it('return OK if args are correct', function() {                  			
+		        it('return OK if args are correct', function() {
           			wod.getone(traceError).should.be.true;
         		});
 
-        		it('return random WOD', function(done) {   
-        			this.timeout(50000);               			
+        		it('return random WOD', function(done) {
+        			this.timeout(50000);
         			wod.getone(function(err,workouts){
-             			if (err) 
+             			if (err)
              				console.log(err);
              			else{
              				var workout = workouts[0];
-	          				workout.should.have.property('title');	
-	          				workout.should.have.property('tagline');	
-	          				workout.should.have.property('exercices');	
+	          				workout.should.have.property('title');
+	          				workout.should.have.property('tagline');
+	          				workout.should.have.property('exercices');
+										workout['exercices'].should.be.ok;
              			}
             			done();
           			});
         		});
 		    });
  		});
-		
+
 
 	}
 )();
